dc3f04cc0f16da3467c21d0e0790703d16882837
Harsh Tiwari <harshtiwari343@email.com>
2024-11-14 10:09:35 +0530

    Updated JSX

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import React from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('demo'));
 
+const flowers = ["Rose", "Dahlia", "Magnolia", "Tulip", "Daisy"];
 // Without JSX
 const element = React.createElement("h1", {}, "Programming with Harsh");
+const element_ = React.createElement(
+    "ul",
+    {},
+    flowers.map((item, index) => React.createElement("li", { key: index }, item))
+)
 
 // With JSX, directly write HTML code inside JS file
-const element2 = <h1>Programming with Harsh</h1>;
+const element2 = <h1>Programming with Harsh 2 + 2 is {2 + 2}</h1>;
+const element2_ = <ul>
+    {flowers.map((item, index) => (
+        <li key={index}>{item}</li>
+    ))
+    }
+</ul>;
 
-root.render(element);
+root.render(element2_);
