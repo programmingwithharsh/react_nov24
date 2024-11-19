7a1b09c6aa2e3fe206cbfb603d92d13404cc5ca1
Harsh Tiwari <harshtiwari343@email.com>
2024-11-19 10:08:26 +0530

    use params from url

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -1,5 +1,8 @@
+import { useParams } from "react-router-dom";
+
 function ProductDetail() {
-    return ( <h1>Product Detail</h1> );
+    let { id } = useParams();
+    return (<h1>Product Detail page, id is {id}</h1>);
 }
 
 export default ProductDetail;
